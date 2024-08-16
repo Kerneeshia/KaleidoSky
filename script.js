e13c3077e05adec6b5b3c9e8d61649841bb55890
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-08-16 15:21:13 +0100

    added more api data

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function updateWeather(response){
     let temperatureElement=document.querySelector("#current-temperature");
     let temperature=response.data.temperature.current;
-    
-let cityElement=document.querySelector("#current-city");
+    let cityElement=document.querySelector("#current-city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windSpeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time * 1000);
+
+
     cityElement.innerHTML=response.data.city;
+    descriptionElement.innerHTML=response.data.condition.description;
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+    timeElement.innerHTML=formatDate(date);
+
+
+
     temperatureElement.innerHTML=Math.round(temperature);
 }
+function formatDate(date){
+let minutes=date.getMinutes();
+let hours=date.getHours();
+let day=[Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday];
 
+let today = day[date.getDay()];
+return `${today} ${hours}:${minutes}`;
+}
 
 function searchCity(city){
 let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
