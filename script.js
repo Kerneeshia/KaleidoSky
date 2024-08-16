80bc0f752fd6c0f935bde617f567bc4b5d207177
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-08-16 12:09:26 +0100

    added basic search engine functionality, no api

---
+++ script.js
@@ -0,0 +1,9 @@
+function citySearchSubmit(event){
+    event.preventDefault();
+let searchInput=document.querySelector("#search-input");
+let cityElement=document.querySelector("#current-city");
+cityElement.innerHTML=searchInput.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", citySearchSubmit);
