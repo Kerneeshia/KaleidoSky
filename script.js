5cf1a23f704cf77312b7727ed416987bfa1f4014
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-08-16 12:59:18 +0100

    ed api

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+function updateWeather(response){
+    let temperatureElement=document.querySelector("#current-temperature");
+    let temperature=response.data.temperature.current;
+    
+let cityElement=document.querySelector("#current-city");
+    cityElement.innerHTML=response.data.city;
+    temperatureElement.innerHTML=Math.round(temperature);
+}
+
+
+function searchCity(city){
+let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
+let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
 function citySearchSubmit(event){
     event.preventDefault();
 let searchInput=document.querySelector("#search-input");
-let cityElement=document.querySelector("#current-city");
-cityElement.innerHTML=searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySearchSubmit);
+
