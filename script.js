d81e53c9effa7a32496534af38330c8a32e0da0c
kerneeshia <kerneeshia.baaitjies@gmail.com>
2024-08-17 11:40:18 +0100

    adding weather data functionality

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function updateWeather(response){
     let humidityElement=document.querySelector("#humidity");
     let windSpeedElement=document.querySelector("#wind-speed");
     let timeElement=document.querySelector("#time");
-    let date=new Date(response.data.time * 1000);
+    let date=new Date(response.data.time);
+    let iconElement =document.querySelector("#icon");
 
 
     cityElement.innerHTML=response.data.city;
@@ -14,23 +15,26 @@ function updateWeather(response){
     humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
     timeElement.innerHTML=formatDate(date);
-
+    iconElement.innerHTML=`<img src="${response.data.condition.icon_url}" alt="" class="current-temperature-icon"/>`
 
 
     temperatureElement.innerHTML=Math.round(temperature);
 }
+
 function formatDate(date){
 let minutes=date.getMinutes();
 let hours=date.getHours();
 let day=[Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday];
-
 let today = day[date.getDay()];
+
 return `${today} ${hours}:${minutes}`;
 }
+
 
 function searchCity(city){
 let apiKey ="a3460f11d3801ff9feb7c65d8btoaadb";
 let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(updateWeather);
 }
 
@@ -45,3 +49,4 @@ searchCity(searchInput.value);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", citySearchSubmit);
 
+searchCity("Johannesburg");
